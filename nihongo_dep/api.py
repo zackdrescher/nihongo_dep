ee3830611a5c6b7af36d5459607181c936d3e616
Zack Drescher <zackryandrescher@gmail.com>
2023-01-30 14:52:47 -0500

    add cors

--- nihongo_dep/api.py
+++ nihongo_dep/api.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from flask_cors import CORS
 
 from . import ginza
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 
 @app.route("/parse/<string:input>")
 def parse(input: str):
-    return dict(text=input, tokens=ginza.GinzaDataFrame.from_text(input))
+    return dict(
+        text=input,
+        tokens=ginza.GinzaDataFrame.from_text(input).to_dict(orient="records"),
+    )
