65b58c6d5c03c1f8d65b7a0e4916a43adde2ec11
Zack Drescher <zackryandrescher@gmail.com>
2023-01-31 16:53:47 -0500

    pull out morph

--- nihongo_dep/ginza.py
+++ nihongo_dep/ginza.py
@@ -40,7 +40,6 @@ class GinzaDataFrame(pd.DataFrame):
         self["orth"] = self.token.apply(lambda token: token.orth_)
         self["lemma"] = self.token.apply(lambda token: token.lemma_)
         self["norm"] = self.token.apply(lambda token: token.norm_)
-        self["morph"] = self.token.apply(lambda token: token.morph)
         self["morph_reading"] = self.token.apply(
             lambda token: token.morph.get("Reading")
         )
